03647a81b08c805fe5587ef9a36dc1b277f906a5
Zuzanna Hypś <susuniak@gmail.com>
2025-05-07 23:25:14 +0200

    Greedy

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "JSSPInstance.h"
 #include "SimpleLoader.h"
 #include "RandomSolver.h"
+#include "GreedySolver.h"
 
 #include <iostream>
 using namespace std;
@@ -8,22 +9,26 @@ using namespace std;
 int main() 
 {
 
-    /*SimpleLoader loader;
+    SimpleLoader loader;
     loader.load("LA/toy.data");
     loader.wypisz();
 
     RandomSolver solver;
     solver.solve(loader.operacje, loader.liczbaJobow, loader.liczbaMaszyn);
     solver.printSchedule();
-    */
 
-    JSSPInstance loader;
+    GreedySolver greedy;
+    greedy.solve(loader.operacje, loader.liczbaJobow, loader.liczbaMaszyn);
+    greedy.printSchedule();
+    
+
+   /* JSSPInstance loader;
     loader.wczytajPlik("Known-Optima/long-js-600000-100-10000-1.data");
     loader.wypiszOperacje();
 
     RandomSolver solver;
     solver.solve(loader.operacje, loader.liczbaJobow, loader.liczbaMaszyn);
-    solver.printSchedule();
+    solver.printSchedule();*/
 
     return 0;
 }
